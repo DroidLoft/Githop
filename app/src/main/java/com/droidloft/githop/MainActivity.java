8441910d00d083652c6fba24bb2bb74bd2e52185
michaelmay <maydev99@gmail.com>
2017-03-19 15:59:29 -0400

    2nd user changes

--- app/src/main/java/com/droidloft/githop/MainActivity.java
+++ app/src/main/java/com/droidloft/githop/MainActivity.java
@@ -1,11 +1,12 @@
 package com.droidloft.githop;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 /*
-**This Is the Original Version*
+**This Is the 2nd user Version 1
 * 3-19-2017 3:32PM
 * :)
  */
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 
     }
