075dbf1149c1528410d3f8fb33b112fc32bb273a
Michael May <maydev99@gmail.com>
2017-03-19 14:58:51 -0400

    Initial commit

---
+++ app/src/main/java/com/droidloft/githop/MainActivity.java
@@ -0,0 +1,21 @@
+package com.droidloft.githop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/*
+**This Is the Original Version*
+* 3-20-2017 2:29PM
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
